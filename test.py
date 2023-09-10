30d9449c57fcf9ddb4514c59c9723c374ca82245
alok <alokkumar991992@gmail.com>
2023-09-11 01:07:25 +0530

    change in test.py model hidden layers

--- test.py
+++ test.py
@@ -23,30 +23,30 @@ print(y_train)
 X_train=X_train/255.0
 X_test=X_test/255.0
 
-# model=tf.keras.models.Sequential([Flatten(input_shape=(28,28)),
-#                                         Dense(784,activation='relu'),
-#                                         Dense(650,activation='relu'),
-#                                         Dense(550,activation='relu'),
-#                                         Dense(450,activation='relu'),
-#                                         Dense(350,activation='relu'),
-#                                         Dense(256,activation='relu'),
-#                                         Dense(128,activation='relu'),
-#                                         Dense(80,activation='relu'),
-#                                         Dense(40,activation='relu'),
+model=tf.keras.models.Sequential([Flatten(input_shape=(28,28)),
                                         
-#                                         Dense(10,activation=tf.nn.softmax)])
+                                        Dense(650,activation='relu'),
+                                        
+                                        Dense(450,activation='relu'),
+                                        
+                                        Dense(250,activation='relu'),
+                                        
+                                        Dense(150,activation='relu'),
+                                        
+                                        Dense(10,activation=tf.nn.softmax)])
 
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-# model.fit(X_train,y_train, epochs=10)
-# model.save("keras_digit_temp.h5")
-# test=X_test[0].reshape(-1,28,28)
-# predicted=model.predict(test)
-# print(predicted)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(X_train,y_train, epochs=10)
+model.save("keras_digit_temp.h5")
+test=X_test[0].reshape(-1,28,28)
+predicted=model.predict(test)
+print(predicted)
 
 def prdict_digit(img):
     loaded_model = keras.models.load_model('keras_digit_temp.h5')
+    
     img_3d=img.reshape(-1,28,28)
     img_resized=img_3d/255.0
     pred_prob=loaded_model.predict(img_resized)
