8a03da328150a683203f56d6ab58d37dca282417
alok <alokkumar991992@gmail.com>
2023-09-11 11:52:39 +0530

    improved version in test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import Dense, Flatten
 import matplotlib.pyplot as plt
 import gradio as gr
 import numpy as np
+import pandas as pd
 #%matplotlib inline
 
 
@@ -23,39 +24,96 @@ print(y_train)
 X_train=X_train/255.0
 X_test=X_test/255.0
 
-model=tf.keras.models.Sequential([Flatten(input_shape=(28,28)),
+# model=tf.keras.models.Sequential([Flatten(input_shape=(28,28)),
                                         
-                                        Dense(650,activation='relu'),
+#                                         Dense(650,activation='relu'),
                                         
-                                        Dense(450,activation='relu'),
+#                                         Dense(450,activation='relu'),
                                         
-                                        Dense(250,activation='relu'),
+#                                         Dense(250,activation='relu'),
                                         
-                                        Dense(150,activation='relu'),
+#                                         Dense(150,activation='relu'),
                                         
-                                        Dense(10,activation=tf.nn.softmax)])
+#                                         Dense(10,activation=tf.nn.softmax)])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit(X_train,y_train, epochs=10)
-model.save("keras_digit_temp.h5")
-test=X_test[0].reshape(-1,28,28)
-predicted=model.predict(test)
-print(predicted)
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+# model.fit(X_train,y_train, epochs=10)
+# model.save("keras_digit_temp.h5")
+# test=X_test[0].reshape(-1,28,28)
+# predicted=model.predict(test)
+# print(predicted)
 
-def prdict_digit(img):
-    loaded_model = keras.models.load_model('keras_digit_temp.h5')
+def predict_digit(img):
+    if img is not None:
+
+        loaded_model = keras.models.load_model('keras_digit_temp.h5')
     
-    img_3d=img.reshape(-1,28,28)
-    img_resized=img_3d/255.0
-    pred_prob=loaded_model.predict(img_resized)
-    predicted_val=np.argmax(pred_prob)
-    return int(predicted_val)
+        simple = pd.DataFrame(
+        {
+        "a": ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+        "b": [28, 55, 43, 91, 81, 53, 19, 87, 52],
+        }
+        )
+        img_3d=img.reshape(-1,28,28)
+        img_resized=img_3d/255.0
+        pred_prob=loaded_model.predict(img_resized)
+    
+        pred_prob=pred_prob*100
 
-iface=gr.Interface(prdict_digit, inputs='sketchpad', outputs='label').launch()
+        print((pred_prob))
+        # prob0= 100*pred_prob[0]
+        # prob1= 100*pred_prob[1]
+        # prob2= 100*pred_prob[2]
+        # prob3= 100*pred_prob[3]
+        # prob4= 100*pred_prob[4]
+        # prob5= 100*pred_prob[5]
+        # prob6= 100*pred_prob[6]
+        # prob7= 100*pred_prob[7]
+        # prob8= 100*pred_prob[8]
+        # prob9= 100*pred_prob[9]
 
-iface.launch(debug='true')
+        # print(prob2) 
+
+    
+        predicted_val=np.argmax(pred_prob)
+        return int(predicted_val)
+        
+    else:
+        return " "
+        
+
+# iface=gr.Interface(prdict_digit, inputs='sketchpad', outputs=['label', gr.Slider(0,100, label='Probably 0'), gr.Slider(0,100, label='Probably 1')] ).launch()
+
+# iface.launch(debug='true')
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            gr.Markdown("Digit Identify", elem_id='title_head')
+            gr.Markdown("By Alok")
+    with gr.Row():
+        with gr.Column():
+            with gr.Row():
+                skch=gr.Sketchpad()
+            with gr.Row():
+                with gr.Column():
+                    clear=gr.ClearButton(skch)
+                with gr.Column():
+                    btn=gr.Button("Identify")
+                    
+        with gr.Column():
+            gr.Markdown("Identified digit")
+            label=gr.Label("")
+            gr.Markdown("Other possible values")
+            #bar=gr.BarPlot("Probability")
+    btn.click(predict_digit,inputs=skch,outputs=label)
+
+           
+        
+    
+demo.launch()
     
 
 
